d1ff45a9fadf1e0b64a6166a3ccff484be9306e4
Heli Julkunen <heli.julkunen@aalto.fi>
2020-05-01 12:43:28 +0300

    Clarify commenting

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 
 def concatenate_features(feature_filenames):
     """
-        This function concatenates horizontally the feature matrices in the files listed
+        This function concatenates the feature matrices in the files listed
         in feature_filenames.
         
         Input:
@@ -15,11 +15,11 @@ def concatenate_features(feature_filenames):
         X: final feature matrix (non-standardized)
         """
     print('Reading file: %s'%feature_filenames[0])
-    X = np.loadtxt("../data/"  + feature_filenames[0] + ".txt") # read the first feature file
+    X = np.loadtxt("data/"  + feature_filenames[0] + ".txt") # read the first feature file
     for i in range(1,len(feature_filenames)):
         filename = feature_filenames[i]
-        print('... reading file: %s'%feature_filenames[i])
-        X_i = np.loadtxt("../data/"  + filename + ".txt")
+        print('Reading file: %s'%feature_filenames[i])
+        X_i = np.loadtxt("data/"  + filename + ".txt")
         X = np.concatenate((X, X_i), axis=1)
     print('... done!')
     return X
@@ -36,7 +36,7 @@ def standardize(X_train, X_test, i_aux):
         X_train: Train set features (one-hot encoding of the tensor structure + auxiliary descriptors)
         X_test: Test set features (one-hot encoding of the tensor structure + auxiliary descriptors)
         i_aux: Index from which the auxiliary descriptors start to be standardized
-               (to exclude the one-hot encodings in the beginning)
+               (to exclude the one-hot encodings from standardization)
                
         Output:
         X_train: Scaled train set features
